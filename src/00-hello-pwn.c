1c0c11143352531843b95fe5a0c9862017badbb8
Ubuntu <ubuntu@ip-172-31-40-18.ap-northeast-2.compute.internal>
2020-12-07 12:46:57 +0000

    Delete pwn

--- src/00-hello-pwn.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-	system("/bin/sh");
-	return EXIT_SUCCESS;
-}
--- src/99-test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int main() {
-	execve("/bin/sh", NULL, NULL);
-	return 0;
-}
